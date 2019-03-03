289058f76c6200bcfd9f7b2d5407cc64ca1eef3e
ffabinhoo <ffabinhoo@gmail.com>
2019-03-03 13:59:09 +1100

    first commit

---
+++ src/components/SimpleMap.component.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+class SimpleMap extends Component {
+  
+  static defaultProps = {
+    centerDefault: {
+      lat: '',
+      lng: ''
+    },
+    zoom: 15
+  };
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lng
+      },
+      zoom: 15
+    };
+}
+
+  render() {
+    
+    return (
+      <div style={{ height: '50vh', width: '50%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key:"AIzaSyD1jLsqcF3_qU89ow-Jm1la8Zt6nF5mTUQ" }}
+          center={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
