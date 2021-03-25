b38c9ddeab95b659c4d6adfb7183c373cc2eb466
Fabio Costa <ffabinhoo@gmail.com>
2021-03-26 10:30:41 +1100

    login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import './App.css';
+import Login from './components/Login';
 import weatherForm from './components/weatherForm.component';
 
 class App extends Component {
@@ -9,11 +10,16 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div className="container">
-          <Route path="/" exact component={weatherForm} />
-        </div>
-      </Router>
+      <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={weatherForm} />
+
+            <Route pth="/login" >
+              <Login />
+            </Route>
+            
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
